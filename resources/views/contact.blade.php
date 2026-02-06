e3008dd3fd96e62630e519a999ee17460045b24f
Akshay Singh <jobakshay21@gmail.com>
2026-02-06 21:13:39 +0530

    routing 101

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Contact Us</title>
+    </head>
+    <body>
+        <h1>Contact Us</h1>
+            <p>We’d love to hear from you! Whether you have a question, feedback, or just want to say hello, feel free to reach out.</p>
+        <div>
+            <ul>
+                <li><a href="/">Return to home</a></li>
+            </ul>
+            <ul>
+                <li><b>Address:</b> 123 Business Park, MG Road, Bengaluru – 560001,India</li>
+                <li><b>Phone:</b> +91 98765 43210</li>
+                <li><b>Email:</b> support@yourcompany.com</li>
+                <li><b>Website:</b> www.yourcompany.com</li>
+            </ul>
+        </div>
+    </body>
+</html>
