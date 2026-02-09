8889d437f6045ad9d7d893511c90e3b0d135b5f0
Akshay Singh <jobakshay21@gmail.com>
2026-02-09 20:25:56 +0530

    card component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,9 @@
     <ul>
         <li><a href="/">Return to home</a></li>
     </ul>
-    <p>We are a passionate team dedicated to delivering high-quality solutions that make a real difference. Founded with a vision to simplify challenges and create meaningful impact, we focus on innovation, integrity, and customer satisfaction in everything we do.</p>
-    <p>Our approach is built on understanding our clients’ needs, combining creativity with expertise, and delivering results that are reliable and scalable. We believe strong relationships, transparency, and continuous improvement are the foundation of long-term success.</p>
-    <p>Whether you’re a growing startup or an established organization, we strive to be a trusted partner on your journey—helping you move forward with confidence.</p>
+    <x-card class="max-w-400">
+        <p>We are a passionate team dedicated to delivering high-quality solutions that make a real difference. Founded with a vision to simplify challenges and create meaningful impact, we focus on innovation, integrity, and customer satisfaction in everything we do.</p>
+        <p>Our approach is built on understanding our clients’ needs, combining creativity with expertise, and delivering results that are reliable and scalable. We believe strong relationships, transparency, and continuous improvement are the foundation of long-term success.</p>
+        <p>Whether you’re a growing startup or an established organization, we strive to be a trusted partner on your journey—helping you move forward with confidence.</p>
+    </x-card>
 </x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ $title }}</title>
+        <style>
+            .card {
+                background: #e3e3e3; padding: 1rem; text-align: center;
+            }
+            .max-w-400 {
+                max-width: 400px;
+                margin: auto;
+            }
+        </style>
     </head>
     <body>
         <nav>
