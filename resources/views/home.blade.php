25a3e33edd817b3945a87c42124a7887686ef524
Akshay Singh <jobakshay21@gmail.com>
2026-02-09 20:39:51 +0530

    pass data to views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
     <h1>Home</h1>
+    {{-- {!! $person !!} --}}
+    {{-- when we are sure that the value is safe --}}
+    <p>{{ $message }}, {{ $person }}!</p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
+Route::view('/', 'home',[
+    'message' => 'Hello',
+    'person' => request('person', 'World'),
+]);
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
