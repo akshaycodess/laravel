e67591d52f0f3929333d27ccbd745f79daa73be9
Akshay Singh <jobakshay21@gmail.com>
2026-02-09 20:55:09 +0530

    blade directives

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,17 @@
 <x-layout>
+    {{-- dd kills execution after that staement and dump dont --}}
+    {{-- @dump($tasks) --}}
+
     <h1>Home</h1>
     {{-- {!! $person !!} --}}
     {{-- when we are sure that the value is safe --}}
     <p>{{ $message }}, {{ $person }}!</p>
+
+    {{-- Blade Directives --}}
+    @forelse ( $tasks as $task )
+        <li>{{ $task }}</li>
+    @empty
+        <p>No tasks right now.</p>
+    @endforelse
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home',[
     'message' => 'Hello',
     'person' => request('person', 'World'),
+    'tasks' => [
+        'task 1',
+        'task 2',
+        'task 3',
+    ],
 ]);
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
